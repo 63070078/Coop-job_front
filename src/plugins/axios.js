db163e712becc2db175864d52ead8081a4ac01aa
tnploy <63070078@kmitl.ac.th>
2024-04-24 09:38:00 +0700

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://coopjob-backend-2nu3pqzog-63070078s-projects.vercel.app/',
+    baseURL: 'http://localhost:3000',
 })
 
 export default instance
